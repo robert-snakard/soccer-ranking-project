2c2b48bab5c413655c2aa561d97434ca01e962d4
Robert Snakard <robert.snakard@gmail.com>
2019-12-18 05:18:27 +0000

    finish add game

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
 import re
+from collections import defaultdict
 
 class App:
     
     pattern = re.compile(r"(?P<teama>.*) (?P<scorea>\d+), (?P<teamb>.*) (?P<scoreb>\d+)")
 
     def __init__(self):
-        self.team_scores = {}
+        self.team_scores = defaultdict(lambda: 0)
 
     def add_game(self, score):
         match = self.pattern.match(score)
         if not match:
             raise InvalidScore
-        print(match.groupdict())
+
+        results = match.groupdict()
+        if int(results["scorea"]) > int(results["scoreb"]):
+            self.team_scores[results["teama"]] += 3;
+        elif int(results["scorea"]) < int(results["scoreb"]):
+            self.team_scores[results["teamb"]] += 3;
+        else:
+            self.team_scores[results["teama"]] += 1;
+            self.team_scores[results["teamb"]] += 1;
 
     def run():
         print("Hello World")
